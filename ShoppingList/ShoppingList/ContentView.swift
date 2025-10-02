bfb7b8bee40f69cea1b44e7f2adbf6ef964e1296
NealCheng <nealpauyol@gmail.com>
2025-10-01 22:04:05 -0400

    feat: init IOS App template

---
+++ ShoppingList/ShoppingList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingList
+//
+//  Created by Boyuan Cheng on 2025-10-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingList/ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Boyuan Cheng on 2025-10-01.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
